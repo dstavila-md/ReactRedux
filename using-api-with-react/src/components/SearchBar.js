51597ab8bbfed986864e9f5436b86680d3cbc6d7
XenderMd <denis.stavilamd@gmail.com>
2025-02-24 20:46:45 +0200

    Handling input elements

--- using-api-with-react/src/components/SearchBar.js
+++ using-api-with-react/src/components/SearchBar.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState('');
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
     onSubmit('cars');
   };
+
+  const onChangeHandler = (event) => {
+    setTerm(event.target.value);
+    console.log(term);
+  };
+
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input />
+        <input onChange={onChangeHandler} value={term} />
       </form>
     </div>
   );
