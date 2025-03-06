4ded6ff82bac35d5b9c621e133108a513dec1549
dstavila <denis.stavila@aspentech.com>
2025-03-06 18:57:09 +0200

    Toggling from display

--- handle-forms/src/components/BookShow.js
+++ handle-forms/src/components/BookShow.js
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
+import BookEdit from "./BookEdit";
+
 function BookShow({book, onDelete}) {
 
-  const handleClick = () => {
+  const [edit, showEdit] = useState(false);
+
+  const handleDeleteClick = () => {
     onDelete(book.id);
+  }
+
+  const handleEditClick = ()=>{
+    showEdit(!edit);
+  }
+
+  let content = <h3>{book.title}</h3>;
+  if(edit){
+    content = <BookEdit/>;
   }
 
   return (
     <div className="book-show">
-      {book.title}
+      <div>{content}</div>
       <div className="actions">
-        <button className="delete" onClick={handleClick}>
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
           Delete
         </button>
       </div>
