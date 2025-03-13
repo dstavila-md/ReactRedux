ccdc6d2bd3bd22089fda978114e4e73c7c9dd36a
dstavila <denis.stavila@aspentech.com>
2025-03-13 19:21:17 +0200

    Fetching a list of books

--- data-persistence-api/src/App.js
+++ data-persistence-api/src/App.js
@@ -6,6 +6,11 @@ import BookList from './components/BookList';
 function App() {
   const [books, setBooks] = useState([]);
 
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  }
+
   const createBook = async (title) => {
     const response = await axios.post('http://localhost:3001/books', { title });
     const updatedBooks = [...books, response.data];
