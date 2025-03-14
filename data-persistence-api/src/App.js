8f934850fdaf924f04b3568a696661f82150b660
XenderMd <denis.stavilamd@gmail.com>
2025-03-14 17:48:15 +0200

    Deleting a record

--- data-persistence-api/src/App.js
+++ data-persistence-api/src/App.js
@@ -36,7 +36,8 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => book.id !== id);
     setBooks(updatedBooks);
   };
