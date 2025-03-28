31bb2c65cf34f2ab2f18f51005b26c54d08bdeeb
XenderMd <denis.stavilamd@gmail.com>
2025-03-28 18:15:18 +0200

    Project setup

---
+++ custom-navigation/src/Button.js
@@ -0,0 +1,7 @@
+// This code defines a functional component named Button
+// that returns a button element with the text "Click me".
+function Button() {
+  return <button>Click me</button>;
+}
+
+export default Button;
--- custom-navigation/src/index.js
+++ custom-navigation/src/index.js
@@ -1,10 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import App from './App';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+root.render(<App />);
