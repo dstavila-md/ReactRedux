1255c8c55e5e89799931fa302e35af28583c515c
XenderMd <denis.stavilamd@gmail.com>
2025-03-28 18:18:36 +0200

    Using the children prop

--- custom-navigation/src/Button.js
+++ custom-navigation/src/Button.js
@@ -1,7 +1,7 @@
 // This code defines a functional component named Button
 // that returns a button element with the text "Click me".
-function Button() {
-  return <button>Click me</button>;
+function Button({ children }) {
+  return <button>{children}</button>;
 }
 
 export default Button;
