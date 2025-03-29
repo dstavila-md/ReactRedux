f898345e365cdda71959dd9edade3a861e8f32aa
XenderMd <denis.stavilamd@gmail.com>
2025-03-29 14:15:42 +0200

    Applying tailwind styling

--- custom-navigation/src/Button.js
+++ custom-navigation/src/Button.js
@@ -11,7 +11,12 @@ function Button({
   outline,
   rounded,
 }) {
-  return <button>{children}</button>;
+  return (
+    // Tailwind CSS classes are used to style the button
+    <button className='px-3 py-1.5 border border-blue-600 bg-blue-500 text-white'>
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
