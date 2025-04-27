08d92618ce359ad76bc47ab54c079fb33c0819e0
XenderMd <denis.stavilamd@gmail.com>
2025-04-27 18:42:53 +0300

    Finding the expanded item

--- custom-navigation-routing/src/components/Accordion.js
+++ custom-navigation-routing/src/components/Accordion.js
@@ -1,5 +1,20 @@
+import useState from 'react';
 function Accordion({ items }) {
-  return <div></div>;
+  const [expandedIndex, setExpandedIndex] = useState(null);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    console.log('isExpanded', isExpanded);
+
+    return (
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+      </div>
+    );
+  });
+  return <div>{renderedItems}</div>;
 }
 
 export default Accordion;
