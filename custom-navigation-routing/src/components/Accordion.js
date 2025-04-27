b1570201af822963d5b129c55d13ea819a07d9f9
XenderMd <denis.stavilamd@gmail.com>
2025-04-27 18:51:26 +0300

    conditional rendering

--- custom-navigation-routing/src/components/Accordion.js
+++ custom-navigation-routing/src/components/Accordion.js
@@ -1,16 +1,15 @@
-import useState from 'react';
+import { useState } from 'react';
+
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(null);
+  const [expandedIndex, setExpandedIndex] = useState(0);
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    console.log('isExpanded', isExpanded);
-
     return (
       <div key={item.id}>
         <div>{item.label}</div>
-        <div>{item.content}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
