34912f9656499fa1852c65756ce9f8e2c02f83dd
XenderMd <denis.stavilamd@gmail.com>
2025-06-02 19:14:06 +0300

    Listening to forward and back browser clicks

--- custom-navigation-routing/src/context/navigation.js
+++ custom-navigation-routing/src/context/navigation.js
@@ -1,14 +1,29 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NavigationContext = createContext({});
 
-const NavigatonProvider = ({ children }) => {
+const NavigationProvider = ({ children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handler);
+
+    return () => {
+      window.removeEventListener('popstate', handler);
+    };
+  }, []);
+
   return (
     <NavigationContext.Provider value={{}}>
+      {currentPath}
       {children}
     </NavigationContext.Provider>
   );
 };
 
-export { NavigatonProvider };
+export { NavigationProvider };
 export default NavigationContext;
