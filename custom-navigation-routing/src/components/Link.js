2efa02bf2e44cf010650fcaf22e10cd7a16facbb
dstavila <denis.stavila@aspentech.com>
2025-06-04 16:40:40 +0300

    Link styling

--- custom-navigation-routing/src/components/Link.js
+++ custom-navigation-routing/src/components/Link.js
@@ -1,9 +1,12 @@
+import classNames from 'classnames';
 import { useContext } from 'react';
 import NavigationContext from '../context/navigation';
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
+  const classes = classNames('text-blue-500')
+  
   const handleClick = (event) => {
     
     if(event.metaKey || event.ctrlKey) {
@@ -15,7 +18,7 @@ function Link({ to, children }) {
     navigate(to);
   };
 
-  return <a href={to} onClick={handleClick}>{children}</a>;
+  return <a className = {classes} href={to} onClick={handleClick}>{children}</a>;
 }
 
 export default Link;
