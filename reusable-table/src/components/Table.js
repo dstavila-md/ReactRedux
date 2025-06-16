7bbd16f1b17f49051d88843a9242116616d7fd9b
XenderMd <denis.stavilamd@gmail.com>
2025-06-16 14:26:25 +0300

    Building the table

--- reusable-table/src/components/Table.js
+++ reusable-table/src/components/Table.js
@@ -1,5 +1,16 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
 }
 
 export default Table;
