b1a770aa94e14fffb57730738117cd16fa49555c
XenderMd <denis.stavilamd@gmail.com>
2025-06-17 20:04:22 +0300

    watching for header cell clicks

--- reusable-table/src/components/SortableTable.js
+++ reusable-table/src/components/SortableTable.js
@@ -2,15 +2,25 @@ import Table from './Table';
 
 function SortableTable(props) {
   const { config } = props;
+
+  const handleClick = (label) => {
+    console.log(label);
+  };
+
   const udpatedConfig = config.map((column) => {
     if (!column.sortValue) return column;
     return {
       ...column,
       header: () => {
-        return <th>{column.label} IS SORTABLE</th>;
+        return (
+          <th onClick={() => handleClick(column.label)}>
+            {column.label} IS SORTABLE
+          </th>
+        );
       },
     };
   });
+
   return <Table {...props} config={udpatedConfig} />;
 }
 
