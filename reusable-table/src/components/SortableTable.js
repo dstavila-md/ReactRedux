c563473aa278c9816631a27e2385c85fb44dc7af
XenderMd <denis.stavilamd@gmail.com>
2025-06-21 19:29:46 +0300

    table state design + sort state

--- reusable-table/src/components/SortableTable.js
+++ reusable-table/src/components/SortableTable.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import Table from './Table';
 
 function SortableTable(props) {
   const { config } = props;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
 
   const handleClick = (label) => {
     console.log(label);
+    if (sortOrder == null) {
+      setSortOrder('asc');
+      setSortBy(label);
+    } else if (sortOrder == 'asc') {
+      setSortOrder('desc');
+      setSortBy(label);
+    } else if (sortOrder == 'desc') {
+      setSortOrder(null);
+      setSortBy(null);
+    }
   };
 
   const udpatedConfig = config.map((column) => {
@@ -21,7 +34,12 @@ function SortableTable(props) {
     };
   });
 
-  return <Table {...props} config={udpatedConfig} />;
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} config={udpatedConfig} />
+    </div>
+  );
 }
 
 export default SortableTable;
