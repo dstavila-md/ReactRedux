1de7f6a6afd51dad5b14df9e87c363d1989b334f
XenderMd <denis.stavilamd@gmail.com>
2025-06-28 17:02:46 +0300

    Implemented a form with a normal hook

--- reducers-intro/src/pages/CounterPage.js
+++ reducers-intro/src/pages/CounterPage.js
@@ -4,6 +4,7 @@ import Panel from '../components/Panel';
 
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -13,6 +14,17 @@ function CounterPage({ initialCount }) {
     setCount(count - 1);
   };
 
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount(count + valueToAdd);
+    setValueToAdd(0); // Reset the input field after adding
+  };
+
   return (
     <Panel className='m-3'>
       <h1 className='text-lg'>Counter: {count}</h1>
@@ -20,9 +32,11 @@ function CounterPage({ initialCount }) {
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
+          value={valueToAdd || ''}
+          onChange={handleChange}
           type='number'
           className='p-1 m-3 bg-gray-50 border border-gray-300'
         />
