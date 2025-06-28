aeb2b57f9ddacfbf426badc87d5f16925e635cbc
XenderMd <denis.stavilamd@gmail.com>
2025-06-28 17:45:56 +0300

    Added constant action types

--- reducers-intro/src/pages/CounterPage.js
+++ reducers-intro/src/pages/CounterPage.js
@@ -2,14 +2,20 @@ import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = 'increment';
+const SET_VALUE_TO_ADD = 'change_value_to_add';
+
 const reducer = (state, action) => {
-  if (action.type === 'increment') {
+  if (action.type === INCREMENT_COUNT) {
     return {
       ...state,
       count: state.count + 1,
     };
-  } else if (action.type === 'add-value') {
-    return { ...state, valueToAdd: action.payload };
+  } else if (action.type === SET_VALUE_TO_ADD) {
+    return {
+      ...state,
+      valueToAdd: action.payload,
+    };
   }
   return state;
 };
@@ -21,7 +27,7 @@ function CounterPage({ initialCount }) {
   });
 
   const increment = () => {
-    dispatch({ type: 'increment', payload: 1 });
+    dispatch({ type: INCREMENT_COUNT, payload: 1 });
   };
 
   const decrement = () => {
@@ -31,7 +37,7 @@ function CounterPage({ initialCount }) {
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
     dispatch({
-      type: 'add-value',
+      type: SET_VALUE_TO_ADD,
       payload: value,
     });
   };
