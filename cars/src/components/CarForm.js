aa38ebf539bcb60d96adf4bb91346079f6fba3c4
XenderMd <denis.stavilamd@gmail.com>
2025-08-01 20:09:45 +0300

    adding an extra reducer

--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -27,32 +27,31 @@ function CarForm() {
 
   return (
     <div className='car-form panel'>
-      <h4 className='subtitle is-3'>
-        <form onSubmit={handleSubmit}>
-          <div className='field-group'>
-            <div className='field'>
-              <label className='label'>Name</label>
-              <input
-                className='input is-expanded'
-                value={name}
-                onChange={handleNameChange}
-              />
-            </div>
-            <div className='field'>
-              <label className='label'>Cost</label>
-              <input
-                className='input is-expanded'
-                type='number'
-                value={cost || ''}
-                onChange={handleCostChange}
-              />
-            </div>
+      <h4 className='subtitle is-3'>Add Car</h4>
+      <form onSubmit={handleSubmit}>
+        <div className='field-group'>
+          <div className='field'>
+            <label className='label'>Name</label>
+            <input
+              className='input is-expanded'
+              value={name}
+              onChange={handleNameChange}
+            />
           </div>
           <div className='field'>
-            <button className='button is-link'>Submit</button>
+            <label className='label'>Cost</label>
+            <input
+              className='input is-expanded'
+              type='number'
+              value={cost || ''}
+              onChange={handleCostChange}
+            />
           </div>
-        </form>
-      </h4>
+        </div>
+        <div className='field'>
+          <button className='button is-link'>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
