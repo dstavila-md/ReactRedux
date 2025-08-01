87a940d11910c42dc992ae330a8219521e675d07
XenderMd <denis.stavilamd@gmail.com>
2025-08-01 19:54:53 +0300

    Listing the records

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -4,7 +4,32 @@ function CarList() {
   const cars = useSelector((state) => {
     return state.cars.carList;
   });
-  return <div>CarList</div>;
+
+  const handleCarDelete = (car) => {};
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className='panel'>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className='button is-danger'
+          onClick={() => {
+            handleCarDelete(car);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div className='car-list'>
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
