7468780f85554f6a0a7de236eeb4273cf9b2b8e0
dstavila <denis.stavila@aspentech.com>
2025-08-05 17:07:48 +0300

    fixed styling

--- media/src/components/UsersListItem.js
+++ media/src/components/UsersListItem.js
@@ -9,12 +9,14 @@ function UsersListItem({ user }) {
 
   return (
     <div className='mb-2 border rounded'>
-      <div className='flex p-2 justify-between items-center cursor-pointer'>
-        <Button loading={isLoading} onClick={handleClick}>
-          <GoTrashcan />
-        </Button>
-        {error && <div>Error removing user...</div>}
-        {user.name}
+      <div className='flex flex-row p-2 justify-between items-center cursor-pointer'>
+        <div className='flex flex-row items-center justify-between'>
+          <Button loading={isLoading} onClick={handleClick} className='mr-3'>
+            <GoTrashcan />
+          </Button>
+          {error && <div>Error removing user...</div>}
+          {user.name}
+        </div>
       </div>
     </div>
   );
