d06c15377263a1bbcd359f8fc7dcb07f4f7d57d0
XenderMd <denis.stavilamd@gmail.com>
2025-08-14 15:56:24 +0300

    Adding the photoApi endpoints and adding a photo

--- media/src/components/PhotosList.js
+++ media/src/components/PhotosList.js
@@ -1,5 +1,25 @@
-function PhotosList() {
-  return 'PhotosList';
+import { useFetchPhotosQuery, useAddPhotoMutation } from '../store';
+import Button from './Button';
+
+function PhotosList({ album }) {
+  const { data, error, isFetching } = useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  return (
+    <div>
+      <div className='m-2 flex flex-row items-center justify-between'>
+        <h3 className='text-lg font-bold'> Photos In {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+          {' '}
+          + Add Photo
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default PhotosList;
