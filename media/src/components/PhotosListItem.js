457093ab9cfc947640708a9b77f955ef7aedca1e
XenderMd <denis.stavilamd@gmail.com>
2025-08-14 16:33:13 +0300

    Adding mouse-over deletes

--- media/src/components/PhotosListItem.js
+++ media/src/components/PhotosListItem.js
@@ -1,7 +1,17 @@
+import { GoTrashcan } from 'react-icons/go';
+import { useRemovePhotoMutation } from '../store';
+
 function PhotosListItem({ photo }) {
+  const [removePhoto] = useRemovePhotoMutation();
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
   return (
-    <div>
-      <img className='h-20 w-20' src={photo.url} alt='random photo' />
+    <div onClick={handleRemovePhoto} className='relative m-2 cursor-pointer'>
+      <img className='h-20 w-20' src={photo.url} alt='random' />
+      <div className='absolute inset-0 items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80'>
+        <GoTrashcan className='text-3xl' />
+      </div>
     </div>
   );
 }
--- media/src/store/apis/photosApi.js
+++ media/src/store/apis/photosApi.js
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { faker } from '@faker-js/faker';
 
 const photosApi = createApi({
   reducerPath: 'photos',
