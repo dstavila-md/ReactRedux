2bba8c230c085ddb0243dfe9fbd29af5b35eafeb
XenderMd <denis.stavilamd@gmail.com>
2025-08-15 18:16:45 +0300

    showing and panning the map

--- google-maps/src/components/Map.tsx
+++ google-maps/src/components/Map.tsx
@@ -1,9 +1,32 @@
+import 'leaflet/dist/leaflet.css';
+import { use, useEffect, useRef } from 'react';
 import type { Place } from '../api/Place';
+import { Map as LeafletMap } from 'leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 interface MapProps {
   place: Place | null;
 }
 
 export default function Map({ place }: MapProps) {
-  return <div>Map {place?.name}</div>;
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current && place) {
+      mapRef.current.flyTo([place.latitude, place.longitude]);
+    }
+  }, [place]);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={[40.7, -74]}
+      zoom={12}
+      scrollWheelZoom
+      className='h-full'
+    >
+      <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+      {place && <Marker position={[place.latitude, place.longitude]} />}
+    </MapContainer>
+  );
 }
