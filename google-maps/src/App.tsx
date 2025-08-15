3d8a38702219a874fcb3e1bd90b86555a0726c59
XenderMd <denis.stavilamd@gmail.com>
2025-08-15 16:34:03 +0300

    component creation

--- google-maps/src/App.tsx
+++ google-maps/src/App.tsx
@@ -1,9 +1,17 @@
+import Map from './components/Map';
+import LocationSearch from './components/LocationSearch';
 
 function App() {
- 
   return (
-    <div>Hi there !</div>
-  )
+    <div className='h-screen w-screen grid grid-cols-12'>
+      <div className='col-span-3 p-2'>
+        <LocationSearch />
+      </div>
+      <div className='col-span-9'>
+        <Map />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
