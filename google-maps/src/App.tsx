2b679748c45c3de221994542355ca8cf98b6defe
XenderMd <denis.stavilamd@gmail.com>
2025-08-15 16:52:08 +0300

    typing a callback

--- google-maps/src/App.tsx
+++ google-maps/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='h-screen w-screen grid grid-cols-12'>
       <div className='col-span-3 p-2'>
-        <LocationSearch />
+        <LocationSearch onPlaceClick={(place) => setPlace(place)} />
       </div>
       <div className='col-span-9'>
         <Map place={place} />
