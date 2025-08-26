4ccb07fb2ea8ee71f63e5e7c1cbb2ddf8943a826
XenderMd <denis.stavilamd@gmail.com>
2025-08-26 22:36:32 +0300

    rendering the list of results

--- registry/src/pages/Root.tsx
+++ registry/src/pages/Root.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 export default function Root() {
   return (
-    <div>
+    <div className='container mx-auto px-20'>
       <Header />
       <Outlet />
     </div>
--- registry/src/pages/search/SearchPage.tsx
+++ registry/src/pages/search/SearchPage.tsx
@@ -1,14 +1,18 @@
 import { useLoaderData } from 'react-router-dom';
 import type { SearchLoaderResult } from './searchLoader';
+import PackageListItem from '../../components/PackageListItem';
 
 export default function SearchPage() {
   const data: SearchLoaderResult = useLoaderData();
 
-  console.log(data);
+  const renderedResults = data.searchResults.map((result) => {
+    return <PackageListItem pack={result} key={result.name} />;
+  });
 
   return (
     <div>
-      <h1>Welcome to the Search Page</h1>
+      <h1 className='text-2xl font-bold my-6'>Search Results</h1>
+      <div className='space-y-4 mt-4'> {renderedResults}</div>
     </div>
   );
 }
