f8c941ce756d77a80b48a50691b34c8461207410
dstavila <denis.stavila@aspentech.com>
2025-08-26 16:31:30 +0300

    More on fetching with a loader

--- registry/src/App.tsx
+++ registry/src/App.tsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './pages/Root';
-import HomePage from "./pages/HomePage";
-import SearchPage from "./pages/SearchPage";
-import DetailsPage from "./pages/DetailsPage";
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import DetailsPage from './pages/DetailsPage';
 
 const router = createBrowserRouter([
   {
@@ -10,14 +10,33 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: '/search', element: <SearchPage />, loader: () => { return ['react', 'react-redux', 'redux']} },
+      {
+        path: '/search',
+        element: <SearchPage />,
+        loader: async ({ request }) => {
+          
+          const { searchParams } = new URL(request.url);
+          const term = searchParams.get('term');
+          
+          if (!term) {
+            throw new Error('Search term must be provided');
+          }
+
+          const res = await fetch(
+            `https://registry.npmjs.org/-/v1/search?text=${term}`
+          );
+
+          const data = await res.json();
+          return data.objects;
+        },
+      },
       { path: '/packages/:name', element: <DetailsPage /> },
     ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
