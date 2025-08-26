3f796284896f7c1356d4233babe2feb003605e1c
dstavila <denis.stavila@aspentech.com>
2025-08-26 15:36:09 +0300

    react router setup

---
+++ registry/src/pages/DetailsPage.tsx
@@ -0,0 +1,7 @@
+export default function DetailsPage() {
+  return (
+    <div>
+      <h1>Welcome to the Details Page</h1>
+    </div>
+  );
+}
---
+++ registry/src/pages/SearchPage.tsx
@@ -0,0 +1,7 @@
+export default function SearchPage() {
+  return (
+    <div>
+      <h1>Welcome to the Search Page</h1>
+    </div>
+  );
+}
