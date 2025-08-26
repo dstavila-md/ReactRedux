da00932c476a00e7d02d0882e4bab148babb6d51
dstavila <denis.stavila@aspentech.com>
2025-08-26 15:57:28 +0300

    form submission navigation shortcut

--- registry/src/components/SearchInput.tsx
+++ registry/src/components/SearchInput.tsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useState } from 'react';
+import { useNavigate, Form } from 'react-router-dom';
 
 export default function SearchInput() {
-  const [term, setTerm] = useState('');
-  const navigate = useNavigate();
+  // const [term, setTerm] = useState('');
+  // const navigate = useNavigate();
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		navigate(`/search?term=${term}`)
-	}
+	// const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+	// 	e.preventDefault();
+	// 	navigate(`/search?term=${term}`)
+	// }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input value={term} onChange={(e) => setTerm(e.target.value)} />
-    </form>
-  );
+  // return (
+  //   <form onSubmit={handleSubmit}>
+  //     <input value={term} onChange={(e) => setTerm(e.target.value)} />
+  //   </form>
+  // );
+
+	return <Form action="/search">
+		<input name="term"/>
+	</Form>
 }
