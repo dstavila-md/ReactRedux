c97e74d1ddcac85d00296d17f55955248380ef88
dstavila <denis.stavila@aspentech.com>
2025-08-27 18:21:21 +0300

    Accessing route parameters

--- registry/src/pages/details/detailsLoader.ts
+++ registry/src/pages/details/detailsLoader.ts
@@ -1,4 +1,19 @@
-export async function detailsLoader(){
-	console.log('Loading package details...');
-	return 'data!!';
+import type { Params } from 'react-router-dom';
+import { getPackage } from '../../api/queries/getPackage';
+
+interface LoaderArgs {
+  params: Params;
+}
+
+
+
+export async function detailsLoader({ params }: LoaderArgs) {
+  const { name } = params;
+  if (!name) {
+    throw new Error('Package name is required');
+  }
+
+  const details = await getPackage(name);
+
+  return { details };
 }
