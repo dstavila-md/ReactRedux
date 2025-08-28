3016f86f0a10422b3ed86683feec842d8d457001
dstavila <denis.stavila@aspentech.com>
2025-08-28 12:23:20 +0300

    Accessing the data from the loader

--- registry/src/pages/home/HomePage.tsx
+++ registry/src/pages/home/HomePage.tsx
@@ -1,4 +1,12 @@
+import type { HomeLoaderResult } from "./homeLoader";
+import { useLoaderData } from "react-router-dom";
+
 export default function HomePage() {
+
+  const {featuredPackages} = useLoaderData() as HomeLoaderResult;
+  
+  console.log(featuredPackages);
+
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
