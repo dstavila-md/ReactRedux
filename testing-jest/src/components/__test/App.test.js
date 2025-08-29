9a071e24b735c68319f693dcf05ff8a43c9fc73f
XenderMd <denis.stavilamd@gmail.com>
2025-08-29 18:09:55 +0300

    JSDOM

--- testing-jest/src/components/__test/App.test.js
+++ testing-jest/src/components/__test/App.test.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 
 import App from '../App';
 
-it('shows a comment box', () => {});
+it('shows a comment box', () => {
+  const div = document.create('div');
+  ReactDOM.render(<App />, div);
+
+  // Looks inside the div
+  // checks to see if the CommentBox is there
+
+  ReactDOM.unmountComponentAtNode(div);
+});
