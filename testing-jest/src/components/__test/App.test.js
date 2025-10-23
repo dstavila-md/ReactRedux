0c0d2de16932d34f5f64ad34b91601fc1799d8f8
XenderMd <denis.stavilamd@gmail.com>
2025-10-23 17:50:04 +0300

    Fixed dependency issue + set expectations for component instances

--- testing-jest/src/components/__test/App.test.js
+++ testing-jest/src/components/__test/App.test.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
+import CommentBox from '../CommentBox';
 import App from '../App';
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-
-  // Looks inside the div
-  // checks to see if the CommentBox is there
-  expect(div.innerHTML).toContain('Comment Box');
-
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
