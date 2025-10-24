b17f31ee61a7748e841e34b0c7a5c2ee96288553
XenderMd <denis.stavilamd@gmail.com>
2025-10-24 15:17:54 +0300

    simulating change events

--- testing-jest/src/components/__test/CommentBox.test.js
+++ testing-jest/src/components/__test/CommentBox.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, simulate } from 'enzyme';
 
 import CommentBox from 'components/CommentBox';
 
@@ -16,4 +16,10 @@ afterEach(() => {
 it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in', () => {
+  wrapped
+    .find('textarea')
+    .simulate('change', { target: { value: 'new string value' } });
 });
