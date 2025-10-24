3ff75dbb7234fa1ef3ca73b5d10bc49ac317292e
XenderMd <denis.stavilamd@gmail.com>
2025-10-24 21:59:52 +0300

    Implemented the provider for the app store

--- testing-jest/src/reducers/index.js
+++ testing-jest/src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import commentsReducer from 'reducers/comments';
-import comments from './comments';
 
 export default combineReducers({
   comments: commentsReducer,
