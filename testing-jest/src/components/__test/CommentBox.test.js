e812beeb0fea1db7a74a15784b530811337213a8
XenderMd <denis.stavilamd@gmail.com>
2025-10-24 15:30:36 +0300

    forcing component updates

--- testing-jest/src/components/__test/CommentBox.test.js
+++ testing-jest/src/components/__test/CommentBox.test.js
@@ -22,4 +22,6 @@ it('has a text area that users can type in', () => {
   wrapped
     .find('textarea')
     .simulate('change', { target: { value: 'new string value' } });
+
+  wrapped.update();
 });
