417043337d85e6e5e4654b5b95400accb55fc3ef
XenderMd <denis.stavilamd@gmail.com>
2025-10-24 13:20:50 +0300

    code reuse with before each

--- testing-jest/src/components/__test/App.test.js
+++ testing-jest/src/components/__test/App.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import CommentBox from '../CommentBox';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 import App from '../App';
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
