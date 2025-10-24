d4347e7147e3624efdedae9facb6dc9aae8e0ca5
XenderMd <denis.stavilamd@gmail.com>
2025-10-24 16:14:00 +0300

    testing form submit

--- testing-jest/src/components/__test/CommentBox.test.js
+++ testing-jest/src/components/__test/CommentBox.test.js
@@ -27,3 +27,22 @@ it('has a text area that users can type in', () => {
 
   expect(wrapped.find('textarea').prop('value')).toEqual('new string value');
 });
+
+it('has a form element', () => {
+  expect(wrapped.find('form').length).toEqual(1);
+});
+
+it('clears the text area when clicking the submit button', () => {
+  wrapped
+    .find('textarea')
+    .simulate('change', { target: { value: 'new string value' } });
+
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('new string value');
+
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
