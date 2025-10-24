03704c4991af1bacccfb51f30c80798ae8cc37cd
XenderMd <denis.stavilamd@gmail.com>
2025-10-24 16:25:06 +0300

    added describe statement

--- testing-jest/src/components/__test/CommentBox.test.js
+++ testing-jest/src/components/__test/CommentBox.test.js
@@ -18,31 +18,25 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  wrapped
-    .find('textarea')
-    .simulate('change', { target: { value: 'new string value' } });
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped
+      .find('textarea')
+      .simulate('change', { target: { value: 'new string value' } });
 
-  wrapped.update();
+    wrapped.update();
+  });
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('new string value');
-});
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new string value');
+  });
 
-it('has a form element', () => {
-  expect(wrapped.find('form').length).toEqual(1);
-});
+  it('clears the text area when clicking the submit button', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new string value');
 
-it('clears the text area when clicking the submit button', () => {
-  wrapped
-    .find('textarea')
-    .simulate('change', { target: { value: 'new string value' } });
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
 
-  wrapped.update();
-
-  expect(wrapped.find('textarea').prop('value')).toEqual('new string value');
-
-  wrapped.find('form').simulate('submit');
-  wrapped.update();
-
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
