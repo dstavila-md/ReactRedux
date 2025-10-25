8895c73fce6b6c5da67850420b303f8312d69507
XenderMd <denis.stavilamd@gmail.com>
2025-10-25 08:54:23 +0300

    Fetching a remote resource (comments) and parsing the comments list

--- testing-jest/src/reducers/comments.js
+++ testing-jest/src/reducers/comments.js
@@ -1,9 +1,16 @@
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 export default function (state = [], action) {
   switch (action.type) {
+    case FETCH_COMMENTS:
+      console.log(action.payload);
+      const comments = action.payload.data.map((comment) => {
+        return comment.name;
+      });
+      return [...state, ...comments];
     case SAVE_COMMENT:
       return [...state, action.payload];
+      break;
     default:
       return state;
   }
