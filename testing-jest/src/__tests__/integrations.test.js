1624c19734a20b9cafd9d38f87d0f353e9c30dcd
XenderMd <denis.stavilamd@gmail.com>
2025-10-25 09:07:18 +0300

    simulating button clicks

--- testing-jest/src/__tests__/integrations.test.js
+++ testing-jest/src/__tests__/integrations.test.js
@@ -5,6 +5,16 @@ import App from 'components/App';
 
 it('can fetch a list of commetns and display them', () => {
   // Attemp to render the entire app
+
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+
   // find the 'fetchComments' button and click it
+
+  wrapped.find('.fetch-comments').simulate('click');
+
   // expect to find a list of comments
 });
