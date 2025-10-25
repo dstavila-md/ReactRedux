d13e9f5b5140ba403c1d453d927023ab44cfdd31
XenderMd <denis.stavilamd@gmail.com>
2025-10-25 07:34:09 +0300

    cheerio queries for testing text content

--- testing-jest/src/components/__test__/CommentList.test.js
+++ testing-jest/src/components/__test__/CommentList.test.js
@@ -17,3 +17,8 @@ beforeEach(() => {
 it('creates one LI per component', () => {
   expect(wrapped.find('li').length).toEqual(2);
 });
+
+it('shows the text for each comment', () => {
+  expect(wrapped.render().text()).toContain('Comment 1');
+  expect(wrapped.render().text()).toContain('Comment 2');
+});
