f8e071c37c42f543a1d2d47a158718c89060f54d
XenderMd <denis.stavilamd@gmail.com>
2025-11-09 17:31:34 +0200

    placing reusable logic

--- testing-jest/src/components/CommentBox.js
+++ testing-jest/src/components/CommentBox.js
@@ -6,23 +6,6 @@ import * as actions from 'actions';
 class CommentBox extends Component {
   state = { comment: '' };
 
-  // Our component just got rendered
-  componentDidMount() {
-    this.shouldNavigateAway();
-  }
-
-  // Our component just got updated
-  componentDidUpdate() {
-    this.shouldNavigateAway();
-  }
-
-  shouldNavigateAway() {
-    if (!this.props.auth) {
-      // Navigate away
-      this.props.history.push('/');
-    }
-  }
-
   handleChange = (event) => {
     this.setState({ comment: event.target.value });
   };
@@ -53,8 +36,4 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return { auth: state.auth };
-}
-
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(CommentBox);
