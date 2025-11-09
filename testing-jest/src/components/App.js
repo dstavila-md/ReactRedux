8ba51ec201d1538a82ff87e02f3edb0688fbd43d
XenderMd <denis.stavilamd@gmail.com>
2025-11-09 16:10:09 +0200

    rendering a header

--- testing-jest/src/components/App.js
+++ testing-jest/src/components/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
-export default () => {
-  return (
-    <div>
-      <Route path='/post' component={CommentBox} />
-      <Route path='/' exact component={CommentList} />
-    </div>
-  );
-};
+export default class App extends Component {
+  renderButton() {}
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/post'>Post a Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path='/post' component={CommentBox} />
+        <Route path='/' exact component={CommentList} />
+      </div>
+    );
+  }
+}
