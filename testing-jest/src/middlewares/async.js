c4df5116f703bf3f2149a37441f328b55274a720
XenderMd <denis.stavilamd@gmail.com>
2025-11-09 19:46:45 +0200

    forwarding actions

--- testing-jest/src/middlewares/async.js
+++ testing-jest/src/middlewares/async.js
@@ -1,3 +1,8 @@
 export default ({ dispatch }) =>
   (next) =>
-  (action) => {};
+  (action) => {
+    // Check to see if the action has a promise on its payload
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+  };
